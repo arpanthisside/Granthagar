bf5b484be17da2a3d8ef10217514e4797a78d57e
arpanthisside <148799147+arpanthisside@users.noreply.github.com>
2024-09-27 18:57:40 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './App.css';
+import './gul.css';
+import Nav from './Nav';
+import Footer from './Footer';
+import Body from './Body';
+function App() {
+  return (
+    <>
+      <Nav />
+      <Body/>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
