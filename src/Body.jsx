2b0bc5514e5a0b11b32e071876b646d07ea786d2
arpanthisside <leetcoder2404@gmail.com>
2024-09-27 20:15:53 +0530

    updated buttons name

--- src/Body.jsx
+++ src/Body.jsx
@@ -16,31 +16,10 @@ const Card = () => {
           </p>
           <div className="d-grid gap-2 d-md-block">
             <button className="btn btn-primary" type="button">
-              Button
+              Read Now
             </button>
             <button className="btn btn-primary" type="button">
-              Button
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <div className="card" style={{ width: '18rem' }}>
-        <img
-          src="https://images.unsplash.com/photo-1551300329-b91a61fa5ebe?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          className="card-img-top"
-          alt="Card image"
-        />
-        <div className="card-body">
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the bulk of the card's content.
-          </p>
-          <div className="d-grid gap-2 d-md-block">
-            <button className="btn btn-primary" type="button">
-              Button
-            </button>
-            <button className="btn btn-primary" type="button">
-              Button
+              Buy
             </button>
           </div>
         </div>
@@ -58,31 +37,10 @@ const Card = () => {
           </p>
           <div className="d-grid gap-2 d-md-block">
             <button className="btn btn-primary" type="button">
-              Button
+            Read Now
             </button>
             <button className="btn btn-primary" type="button">
-              Button
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <div className="card" style={{ width: '18rem' }}>
-        <img
-          src="https://images.unsplash.com/photo-1551300329-b91a61fa5ebe?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          className="card-img-top"
-          alt="Card image"
-        />
-        <div className="card-body">
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the bulk of the card's content.
-          </p>
-          <div className="d-grid gap-2 d-md-block">
-            <button className="btn btn-primary" type="button">
-              Button
-            </button>
-            <button className="btn btn-primary" type="button">
-              Button
+            Buy
             </button>
           </div>
         </div>
@@ -100,10 +58,10 @@ const Card = () => {
           </p>
           <div className="d-grid gap-2 d-md-block">
             <button className="btn btn-primary" type="button">
-              Button
+            Read Now
             </button>
             <button className="btn btn-primary" type="button">
-              Button
+            Buy
             </button>
           </div>
         </div>
@@ -121,10 +79,52 @@ const Card = () => {
           </p>
           <div className="d-grid gap-2 d-md-block">
             <button className="btn btn-primary" type="button">
-              Button
+            Read Now
             </button>
             <button className="btn btn-primary" type="button">
-              Button
+            Buy
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="card" style={{ width: '18rem' }}>
+        <img
+          src="https://images.unsplash.com/photo-1551300329-b91a61fa5ebe?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          className="card-img-top"
+          alt="Card image"
+        />
+        <div className="card-body">
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <div className="d-grid gap-2 d-md-block">
+            <button className="btn btn-primary" type="button">
+            Read Now
+            </button>
+            <button className="btn btn-primary" type="button">
+            Buy
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="card" style={{ width: '18rem' }}>
+        <img
+          src="https://images.unsplash.com/photo-1551300329-b91a61fa5ebe?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          className="card-img-top"
+          alt="Card image"
+        />
+        <div className="card-body">
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <div className="d-grid gap-2 d-md-block">
+            <button className="btn btn-primary" type="button">
+            Read Now
+            </button>
+            <button className="btn btn-primary" type="button">
+            Buy
             </button>
           </div>
         </div>
